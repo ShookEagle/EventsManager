1cd0ebd1ccda41b9e34942afd92292833202dda8
ShookEagle <shookeagle@gmail.com>
2025-06-04 16:50:56 -0500

    delete unused model

--- EventsManager/src/plugin/models/CommandPayload.cs
+++
@@ -1,9 +0,0 @@
-namespace EventsManager.plugin.models;
-
-public class CommandPayload
-{
-    public string Type { get; set; }
-    public string Executor { get; set;  }
-    public string Target { get; set; }
-    public string? Reason { get; set; }
-}
