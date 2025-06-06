dc3cb4fd2f4b62f4bbaf563ab0dcd17c00810e38
ShookEagle <shookeagle@gmail.com>
2025-06-06 14:18:18 -0500

    Add Empty Constructors

--- EventsManager/src/plugin/models/GameMode.cs
+++ EventsManager/src/plugin/models/GameMode.cs
@@ -1,23 +1,25 @@
 namespace EventsManager.plugin.models;
 
-public abstract class GameMode
+public class GameMode
 {
     public string MapGroup { get; set; } = "Active";
     public string Description { get; set; } = "";
     public string Tags { get; set; } = "";
-    public List<string> Plugins { get; set; } = [];
-    public List<string> CommandPacks { get; set; } = [];
+    public List<string>? Plugins { get; set; } = [];
+    public List<string>? CommandPacks { get; set; } = [];
     public GameModeSettings Settings { get; set; } = new();
+    
+    public GameMode() {}
 }
 
 public class GameModeSettings
 {
-    public Dictionary<string, object>? Generic { get; set; }
-    public Dictionary<string, object>? Roundflow { get; set; }
-    public Dictionary<string, object>? Bots { get; set; }
-    public Dictionary<string, object>? Movement { get; set; }
-    public Dictionary<string, object>? Weapons { get; set; }
-    public Dictionary<string, object>? Players { get; set; }
-    public Dictionary<string, object>? Communication { get; set; }
+    public Dictionary<string, object?>? Generic { get; set; }
+    public Dictionary<string, object?>? Roundflow { get; set; }
+    public Dictionary<string, object?>? Bots { get; set; }
+    public Dictionary<string, object?>? Movement { get; set; }
+    public Dictionary<string, object?>? Weapons { get; set; }
+    public Dictionary<string, object?>? Players { get; set; }
+    public Dictionary<string, object?>? Communication { get; set; }
     public List<string>? CustomCommands { get; set; }
 }
--- EventsManager/src/plugin/models/MapGroup.cs
+++ EventsManager/src/plugin/models/MapGroup.cs
@@ -1,9 +1,12 @@
 namespace EventsManager.plugin.models;
 
-public class MapGroup : List<MapInfo> { }
+public class MapGroup : List<MapInfo>
+{
+    public MapGroup() { }
+}
 
-public abstract class MapInfo
+public class MapInfo
 {
     public string Name { get; set; } = string.Empty;
-    public string WorkshopId { get; set; } = string.Empty;
+    public string? WorkshopId { get; set; } = string.Empty;
 }
