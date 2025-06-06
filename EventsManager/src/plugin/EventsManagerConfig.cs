20bd102d7a07f18296d49d79130c16f8140a6054
ShookEagle <shookeagle@gmail.com>
2025-06-06 11:43:24 -0500

    update server Webhost data

--- EventsManager/src/plugin/EventsManagerConfig.cs
+++ EventsManager/src/plugin/EventsManagerConfig.cs
@@ -5,6 +5,6 @@ namespace EventsManager.plugin;
 
 public class EventsManagerConfig : BasePluginConfig
 {
-    [JsonPropertyName("WEB_SERVER_URL")] public string? WebServerUrl { get; set; }
-    [JsonPropertyName("WEB_SERVER_PORT")] public string? WebServerPort { get; set; }
+    [JsonPropertyName("WEB_SERVER_URL")] public string? WebServerUrl { get; set; } = "http://localhost";
+    [JsonPropertyName("WEB_SERVER_PORT")] public string? WebServerPort { get; set; } = "8000";
 }
