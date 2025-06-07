c88ed37a578940a93b1fb1c10b46ce2e8a96b1aa
ShookEagle <shookeagle@gmail.com>
2025-06-07 13:30:52 -0500

    Create Styles

---
+++ EventsManager/src/plugin/menus/components/Elements.cs
@@ -0,0 +1,31 @@
+using EventsManager.plugin.menus.style;
+using RMenu;
+
+namespace EventsManager.plugin.menus.components;
+
+public static class Elements
+{
+    public static readonly MenuValue[] Cursor =
+    {
+        new("►", Theme.PrimaryBlue),
+        new("◄", Theme.PrimaryBlue)
+    };
+
+    public static readonly MenuValue[] Selector =
+    {
+        new("[ ", Theme.PrimaryBlue),
+        new(" ]", Theme.PrimaryBlue)
+    };
+
+    public static readonly MenuValue[] Footer =
+    {
+        new("Scroll - ",    Theme.PlaceholderText),
+        new("W/S",          Theme.AccentBlue),
+        new("| Select - ",  Theme.PlaceholderText),
+        new("Jump",         Theme.AccentGreen),
+        new("| Back - ",    Theme.PlaceholderText),
+        new("Crouch",       Theme.AccentYellow),
+        new("| Exit - ",    Theme.PlaceholderText),
+        new("Tab",          Theme.AccentDarkRed)
+    };
+}
---
+++ EventsManager/src/plugin/menus/style/Theme.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+
+namespace EventsManager.plugin.menus.style;
+
+public static class Theme
+{
+    public static readonly Color TextPrimary        = Color.FromArgb(255, 255, 255);    // white
+    public static readonly Color PlaceholderText    = Color.FromArgb(136, 136, 136);    // #888
+    public static readonly Color TextDark           = Color.FromArgb(68, 68, 68);       // #444
+    
+    public static readonly Color PrimaryBlue        = Color.FromArgb(58, 110, 165);     // #3a6ea5
+    public static readonly Color AccentBlue         = Color.FromArgb(93, 157, 253);     // #5d9dfd
+    public static readonly Color AccentYellow       = Color.FromArgb(244, 197, 66);     // #f4c542
+    public static readonly Color AccentGreen        = Color.FromArgb(59, 165, 93);      // #3ba55d
+    public static readonly Color AccentRed          = Color.FromArgb(255, 102, 102);    // #ff6666
+    public static readonly Color AccentDarkRed      = Color.FromArgb(255, 51, 51);      // #ff3333
+}
