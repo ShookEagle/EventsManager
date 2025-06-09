dca1e8fdcbdae7321927bd189d581168b889cd8a
ShookEagle <shookeagle@gmail.com>
2025-06-08 20:29:56 -0500

    Remove Announcer Conditions

--- EventsManager/src/plugin/services/AnnouncerService.cs
+++ EventsManager/src/plugin/services/AnnouncerService.cs
@@ -10,8 +10,7 @@ public class AnnouncerService(IEventsManager plugin) : IAnnouncerService
 {
     public void Announce(string admin, string target, string action,
         string suffix = "", string actionColor = "bluegrey") {
-        foreach (var player in
-                 Utilities.GetPlayers().Where(player => admin != player.PlayerName)) {
+        foreach (var player in Utilities.GetPlayers()) {
             player.PrintLocalizedChat(plugin.GetBase().Localizer,
                 "admin_action_format",
                 player.GetRank() >= MaulPermission.EG ? admin : "EC", actionColor, action, target,
