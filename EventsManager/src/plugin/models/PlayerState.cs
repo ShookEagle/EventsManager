bc09b076208fa6075272e0b812e310fa6facef5d
ShookEagle <shookeagle@gmail.com>
2025-06-11 14:24:12 -0500

    Create PlayerState model

--- EventsManager/src/plugin/models/PlayerState.cs
+++ EventsManager/src/plugin/models/PlayerState.cs
@@ -1,12 +1,13 @@
-using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API;
+using EventsManager.plugin.extensions;
 
 namespace EventsManager.plugin.models;
 
-public abstract class PlayerState
+public class PlayerState(ulong steamId, string name)
 {
-    public string Name { get; set; } = "";
-    public string Team { get; set; } = "spectator";
-    public string Rank { get; set; } = "";
-    public bool Muted { get; set; } = false;
+    public ulong SteamId { get; init; } = steamId;
+    public string Name { get; set; } = name;
+    public string Team { get; set; } = Utilities.GetPlayerFromSteamId(steamId)!.Team.ToString();
+    public string Rank { get; set; } = Utilities.GetPlayerFromSteamId(steamId)!.GetRank().ToString();
     public long JoinTime { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 }
